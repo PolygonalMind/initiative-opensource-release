126660a13be3b4d71894b4312349f5065d98df05
Lau-alt <laura@polygonalmind.com>
2023-03-31 13:32:08 +0200

    Update unity project and Medieval  fair assets inside of it

---
+++ Opensource_Unity/Assets/Polygonal Mind/Tools/Editor/PolygonalMind_Overlay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+#if UNITY_EDITOR
+
+public class PolygonalMind_Overlay : MonoBehaviour
+{
+    //storing  paths so they can be easily modified and accessed
+    private const string PolygonalMenuPath = "Polygonal Mind/";
+    private const string PublisherPath = PolygonalMenuPath + "Publisher Page";
+    private const string GithubPath = PolygonalMenuPath + "Github";
+    private const string WebPath = PolygonalMenuPath + "About Polygonal Mind";
+
+    /*
+    MenuItem(path, priority) 
+    the smaller the prio number, the higher it goes at the menu bar. If empty, random. A bar is included if prio difference >10
+    */
+    [MenuItem(PublisherPath, priority = 1)]
+    private static void AssetStoreAccess() 
+    {
+	    Application.OpenURL("https://assetstore.unity.com/publishers/36722");
+    }
+
+    [MenuItem(GithubPath, priority = 2)]
+    private static void GithubAccess() 
+    {
+	    Application.OpenURL("https://github.com/PolygonalMind");
+    }
+
+    [MenuItem(WebPath, priority = 13)]
+    private static void WebAccess() 
+    {
+	    Application.OpenURL("https://www.polygonalmind.com/");
+    }
+
+}
+
+#endif
